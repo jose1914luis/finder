73bd467f2533eb1598eb1ea08455116c7bf130f3
jose luis <jose1914luis@gmail.com>
2017-05-22 19:03:41 -0500

    accesibilidad garantizada

--- Vistas/indexar.digitalizar.v.php
+++ Vistas/indexar.digitalizar.v.php
@@ -1,38 +1,39 @@
 <script>
-    function selFormulario(idPlantilla, codigoExp ) {
+    function selFormulario(idPlantilla, codigoExp) {
         //console.log('viewFormularios.php?idPlantilla=' + idPlantilla + "&codigoExp =" + codigoExp);
 //        idEmpresa = document.frm01.selEmpresa.options[document.frm01.selEmpresa.selectedIndex].value;
 //        if (idEmpresa > 0)
-            $("#template").load('viewFormularios.php?idPlantilla=' + idPlantilla + "&codigoExp=" + codigoExp);
+        $("#template").load('viewFormularios.php?idPlantilla=' + idPlantilla + "&codigoExp=" + codigoExp);
 //        else
 //            alert("Debe seleccionar una Empresa");
     }
     ;
 </script>
 
+
 <form name="frm01" id="frm01" method="POST" enctype="multipart/form-data" class="form-horizontal">
     <input type="hidden" name="operacionForm" value="indexar.digitalizar"/>
-        
-    
+
+
     <div class="form-group">
         <label class="control-label col-sm-3" for="email"> Selección de la plantilla respectiva:</label>
         <div class="col-sm-5">
             <?php if (!empty($listaPlantillas) && !empty($listaEmpresas)) { ?>
-            <select name="selPlantilla" class="form-control" id="selPlantilla" onchange="selFormulario(this.value, <?= "'". $codigoExp . "'" ?>)" >
-                        <option value="0" selected="selected">Seleccione la plantilla
-                            <?php
-                            foreach ($listaPlantillas as $cadaPlantilla)
-                                echo "<option value='" . $cadaPlantilla["id"] . "' title='" . utf8_decode($cadaPlantilla["detalle"]) . "'>" . strtoupper(utf8_decode($cadaPlantilla["nombre"])) . "</option>\n";
-                            ?>	                        
-                    </select>				
-                <?php } else { ?>
-            <center><h2>No Existen Plantillas en el Sistema. Primero debe generar plantillas en Keeper</h2></center>
-        <?php } ?>		
+                <select name="selPlantilla" class="form-control" id="selPlantilla" onchange="selFormulario(this.value, <?= "'" . $codigoExp . "'" ?>)" >
+                    <option value="0" selected="selected">Seleccione la plantilla
+                        <?php
+                        foreach ($listaPlantillas as $cadaPlantilla)
+                            echo "<option value='" . $cadaPlantilla["id"] . "' title='" . utf8_decode($cadaPlantilla["detalle"]) . "'>" . strtoupper(utf8_decode($cadaPlantilla["nombre"])) . "</option>\n";
+                        ?>	                        
+                </select>				
+            <?php } else { ?>
+                <center><h2>No Existen Plantillas en el Sistema. Primero debe generar plantillas en Keeper</h2></center>
+            <?php } ?>		
         </div>
     </div>
 
     <table border="0" align="center" width="100%">
-        	
+
         <tr>
             <td width="100%" align="left" valign="top">
 
