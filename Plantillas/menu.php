ac41eb83b7b139e0aeaeb8d59f426a0726c18120
jose luis <jose1914luis@gmail.com>
2017-06-22 17:53:59 -0500

    accesibilidad garantizada

---
+++ Plantillas/menu.php
@@ -0,0 +1,48 @@
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">                    
+
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href=".">
+                <img class="logosgm" alt="SIGMIN" src="Javascript/images/logo_login.png" width="50%">
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+<!--            
+            <form class="navbar-form navbar-left">
+                <div class="input-group buscador">
+                    <input type="text" class="form-control" placeholder="Palabra Clave...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                    </span>
+                </div> /input-group 
+            </form>            -->
+
+            <ul class="nav navbar-nav navbar-right">
+<!--                <li><a href="#"> Creditos: 1000<span class="glyphicon glyphicon-usd"></span></a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Propiedades Mineras <span class="caret"></span></a>
+                    <ul class="dropdown-menu">                                    
+                        <li><a href="#">Mis Prospectos</a></li>
+                        <li><a href="#">Mis Expedientes</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Liberaciones</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Shapes</a></li>
+                    </ul>
+                </li>-->
+                <li><a href=".?ope=ingresar">Ingresar <span class="glyphicon glyphicon-user"></span></a></li>                                                        
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
