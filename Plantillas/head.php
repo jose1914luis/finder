ee13fd604e1ff576bfb7e2b04a5f9e61013d7398
jose luis <jose1914luis@gmail.com>
2017-06-22 17:54:31 -0500

    accesibilidad garantizada

--- Plantillas/head.php
+++ Plantillas/head.php
@@ -6,8 +6,8 @@
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="Javascript/jquery.placeholder.min.js"></script>   
-<script src="Javascript/procesarUsrLogin.js?v=<?= time() ?>"></script>   
-<link rel="stylesheet" href="Javascript/login.css?v=<?= time() ?>" type="text/css" media="all" />
+<script src="Javascript/procesarUsrLogin.js?v=<?= 3 ?>"></script>   
+<link rel="stylesheet" href="Javascript/login.css?v=<?= 3 ?>" type="text/css" media="all" />
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
