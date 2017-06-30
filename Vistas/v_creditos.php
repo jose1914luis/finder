b91869d8d9e3addc1089ecb91ad38122b1a41371
jose luis <jose1914luis@gmail.com>
2017-06-29 19:45:43 -0500

    modificaciones nueva version

--- Vistas/v_creditos.php
+++ Vistas/v_creditos.php
@@ -1,128 +1,109 @@
 <?php
+if (empty($_GET["pagina"])) {
+    $listaCreditosHistorial = $cred->getCreditosHistoricosByIdUsuario($_SESSION['id_usuario']);
+    //$prospects 								= $prp->selectProspectosByUsuario($_SESSION["id_usuario"]);
+    // Para paginación del resultado
+    $max_por_pagina = $GLOBALS ["max_por_pagina"];
+    $_SESSION["lista_credito"]["paginas"] = (!empty($listaCreditosHistorial)) ? array_chunk($listaCreditosHistorial, $max_por_pagina) : null;
+    $_SESSION["lista_credito"]["caption"] = "Reporte de Cr&eacute;ditos Consumidos";
+}
 
-	if(empty($_GET["pagina"])) {
-		$listaCreditosHistorial 					= $cred->getCreditosHistoricosByIdUsuario($_SESSION['id_usuario']);
-		//$prospects 								= $prp->selectProspectosByUsuario($_SESSION["id_usuario"]);
+$total_paginas = sizeof(@$_SESSION["lista_credito"]["paginas"]);
 
-		// Para paginación del resultado
-		$max_por_pagina 							= $GLOBALS ["max_por_pagina"];	
-		$_SESSION["lista_credito"]["paginas"] 		= (!empty($listaCreditosHistorial)) ? array_chunk($listaCreditosHistorial, $max_por_pagina) : null; 
-		$_SESSION["lista_credito"]["caption"] 		= "Reporte de Cr&eacute;ditos Consumidos";	
-	}			
+$pagina_actual = (@$_GET["pagina"] > 1) ? $pagina_actual = $_GET["pagina"] : 1;
+$dataSET = @$_SESSION["lista_credito"]["paginas"][$pagina_actual - 1];
 
-	$total_paginas 		= sizeof(@$_SESSION["lista_credito"]["paginas"]);	
+// generación de listado de paginación, paginas por pantalla:
+$pags_pantalla = $GLOBALS ["pags_pantalla"];
 
-	$pagina_actual 		= (@$_GET["pagina"] > 1) ? 	$pagina_actual = $_GET["pagina"] : 1;
-	$dataSET			= @$_SESSION["lista_credito"]["paginas"][$pagina_actual - 1];
-	
-	// generación de listado de paginación, paginas por pantalla:
-	$pags_pantalla		= $GLOBALS ["pags_pantalla"];
-			
-	function generaPaginacion($url, $pags_pantalla, $pagina_actual, $total_paginas) {			
-		$pagInicial	= floor(($pagina_actual-1)/$pags_pantalla)*$pags_pantalla + 1;
-		$pagFinal	= ($pagInicial+$pags_pantalla-1>$total_paginas)? $total_paginas : $pagInicial+$pags_pantalla-1;
+function generaPaginacion($url, $pags_pantalla, $pagina_actual, $total_paginas) {
+    $pagInicial = floor(($pagina_actual - 1) / $pags_pantalla) * $pags_pantalla + 1;
+    $pagFinal = ($pagInicial + $pags_pantalla - 1 > $total_paginas) ? $total_paginas : $pagInicial + $pags_pantalla - 1;
 
-		$pagAnt		= ($pagInicial-1<1) ? 1 : $pagInicial-1;
-		$pagSte		= ($pagFinal+1 > $total_paginas) ?  $total_paginas : $pagFinal+1; 
-		
-		$paginacion = '<ul class="pagination"><li><a href="'.$url.'&pagina='.$pagAnt.'">«</a></li>';
-		for($i=$pagInicial; $i <= $pagFinal; $i++) {
-			$activar = '';
-			if($i==$pagina_actual) $activar=' class="active" ';
-			$paginacion .= '<li><a '.$activar.' href="'.$url.'&pagina='.$i.'">'.$i.'</a></li>'; 
-		} 
-		$paginacion .= '<li><a href="'.$url.'&pagina='.$pagSte.'">»</a></li></ul>';
-		return 	$paginacion;
-	}
+    $pagAnt = ($pagInicial - 1 < 1) ? 1 : $pagInicial - 1;
+    $pagSte = ($pagFinal + 1 > $total_paginas) ? $total_paginas : $pagFinal + 1;
 
-	// fin de variables de paginación del resultado
-	if(!empty($dataSET))	$tfoot = generaPaginacion("?mnu=creditos", $pags_pantalla, $pagina_actual, $total_paginas, $max_por_pagina);
-	else					$tfoot = "<b><i>No reporta consumo de Cr&eacute;ditos</i></b>";
+    $paginacion = '<ul class="pagination"><li><a href="' . $url . '&pagina=' . $pagAnt . '">«</a></li>';
+    for ($i = $pagInicial; $i <= $pagFinal; $i++) {
+        $activar = '';
+        if ($i == $pagina_actual)
+            $activar = ' class="active" ';
+        $paginacion .= '<li><a ' . $activar . ' href="' . $url . '&pagina=' . $i . '">' . $i . '</a></li>';
+    }
+    $paginacion .= '<li><a href="' . $url . '&pagina=' . $pagSte . '">»</a></li></ul>';
+    return $paginacion;
+}
 
+// fin de variables de paginación del resultado
+if (!empty($dataSET))
+    $tfoot = generaPaginacion("?mnu=creditos", $pags_pantalla, $pagina_actual, $total_paginas, $max_por_pagina);
+else
+    $tfoot = "<b><i>No reporta consumo de Cr&eacute;ditos</i></b>";
 ?>
 
-<form name="frmCreditos" method="post" action="?mnu=creditos_compra" target="_blank">
-	<table width="95%" border="0" align="center" cellspacing="0" class="tableFonts">
-		<tr>
-			<td colspan="2" class="titleSite" align='center'>Mis Cr&eacute;ditos</td>
-		</tr>
-		<tr>
-			<td colspan="2">&nbsp;</td>
-		</tr>
-		<tr bgcolor="yellow">
-			<td><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Total de Cr&eacute;ditos disponibles:<b/></td>
-			<td >
-				<div id="totalCreditos"><h3><font color="green"><?=$_SESSION['usr_cred']['credito']?> Cr&eacute;dito(s)</font></h3></div>
-			</td>
-		</tr>	
-		<tr>
-			<td colspan="2"><hr size="0"/></td>
-		</tr>
-		<tr>
-			<td><b>Compra de nuevos cr&eacute;ditos (1 Cr&eacute;dito <=> 1000 COP):</b></td>
-			<td valign="middle">
-				<table border="0">
-					<tr>
-						<td><input type="text" name="txtCompraCreditos" placeholder="Mínimo <?=$GLOBALS ["SIGCoin_minimo"]?> COP" onchange="this.value=Math.floor(this.value/1000)*1000"/></td>
-						<td>&nbsp;&nbsp;</td>
-						<td><a href="javascript:" onclick="document.frmCreditos.submit()"/><img src="Imgs/enviarPago.png" border="0" width="181" height="39"></a></td>
-					</tr>
-				</table>
-		</tr>
-		<tr>
-			<td colspan="2"><hr size="0"/></td>
-		</tr>				
-		<tr>
-			<td colspan="2">
-				<table class="results" align="center" width="100%">	
-					<caption>
-						<div class="titleSite" style="text-align:center">Reporte de Cr&eacute;ditos</div>
-						<div>&nbsp;</div>				
-					</caption>
-					<tfoot>
-						<tr>
-							<td colspan="6" align="center">
-								<ul class="pagination">
-									<?=$tfoot?>
-								</ul>					
-							</td>
-						</tr>
-					</tfoot>
-					<tbody>
-					<tr class="results">
-						<th class="results">Descarga</th>
-						<th class="results">Producto</th>
-						<th class="results">Placa</th>
-						<th class="results">Cr&eacute;ditos Consumidos</th>
-						<th class="results">Fecha Consumo</th>
-						<th class="results">Fecha Vence</th>
-					</tr>
-					
-		<?php
-			if(!empty($dataSET))
-				foreach($dataSET as $cadaCredito) {
-					$cadaClasificacion = (@$clasificacion[$cadaCredito["producto"]][$cadaCredito["tipo_expediente"]]!="") ? "&clasificacion=".$clasificacion[$cadaCredito["producto"]][$cadaCredito["tipo_expediente"]] : "";
-					
-					$target = 'target="_blank"';
-					if($cadaCredito["url_descarga"] == "?crd=liberaciones")
-						$target = "";
-		?>
-					<tr class="results">
-						<td align="center" class="results"><center>
-							<a href="<?=$cadaCredito["url_descarga"]?>&creditos_prod=<?=$cadaCredito["credito_prod"]?>&placa=<?=$cadaCredito["placa"]?><?=$cadaClasificacion?>" title="Descarga del producto" <?=$target?>><img src='Imgs/reportIcon.png' border='0' width='30' height='30'></center></a>
-						</td>					
-						<td class="results"><?=$cadaCredito["producto"]?></td>
-						<td class="results"><?=$cadaCredito["placa"]?></td>
-						<td class="results"><?=$cadaCredito["creditos_consumidos"]?> Cr&eacute;dito(s)</td>
-						<td class="results"><?=$cadaCredito["fecha_generacion"]?></td>
-						<td class="results"><?=$cadaCredito["fecha_vence"]?></td>
-					</tr>					
-		<?php
-				}
-		?>		
-					</tbody>	
-				</table>
-			</td>
-		</tr>			
-	</table>	
-</form>
+<div class="panel panel-primary">
+    <!-- Default panel contents -->
+    <div class="panel-heading"><h4>Mis Créditos</h4></div>
+    <div class="panel-body">
+
+        <center><h4><b>Total de Cr&eacute;ditos disponibles:</b> <?= $_SESSION['usr_cred']['credito'] ?> Cr&eacute;dito(s)</h4></center>
+
+        <hr>
+        <form name="frmCreditos" method="post" action="?mnu=creditos_compra" target="_blank" class="form-inline">
+            <div class="form-group">
+                <label for="exampleInputName2">Compra de nuevos cr&eacute;ditos (1 Cr&eacute;dito <=> 1000 COP):</label>
+                <input type="text"  class="form-control" name="txtCompraCreditos" placeholder="Mínimo <?= $GLOBALS ["SIGCoin_minimo"] ?> COP" onchange="this.value = Math.floor(this.value / 1000) * 1000"/>
+
+            </div>           
+            <button type="button" onclick="document.frmCreditos.submit()" class="btn btn-success">
+                Enviar Pago <span class="glyphicon glyphicon-credit-card"></span>
+            </button>
+        </form>
+
+    </div>
+    <div class="titleSite" style="text-align:center">Reporte de Cr&eacute;ditos</div>
+    <table class="table table-striped" align="center" width="100%">	
+        <tfoot>
+            <tr>
+                <td colspan="6" align="center">
+                    <ul class="pagination">
+                        <?= $tfoot ?>
+                    </ul>					
+                </td>
+            </tr>
+        </tfoot>
+        <tbody>
+            <tr class="results">
+                <th class="results">Descarga</th>
+                <th class="results">Producto</th>
+                <th class="results">Placa</th>
+                <th class="results">Cr&eacute;ditos Consumidos</th>
+                <th class="results">Fecha Consumo</th>
+                <th class="results">Fecha Vence</th>
+            </tr>
+
+            <?php
+            if (!empty($dataSET))
+                foreach ($dataSET as $cadaCredito) {
+                    $cadaClasificacion = (@$clasificacion[$cadaCredito["producto"]][$cadaCredito["tipo_expediente"]] != "") ? "&clasificacion=" . $clasificacion[$cadaCredito["producto"]][$cadaCredito["tipo_expediente"]] : "";
+
+                    $target = 'target="_blank"';
+                    if ($cadaCredito["url_descarga"] == "?crd=liberaciones")
+                        $target = "";
+                    ?>
+                    <tr class="results">
+                        <td align="center" class="results"><center>
+                    <a href="<?= $cadaCredito["url_descarga"] ?>&creditos_prod=<?= $cadaCredito["credito_prod"] ?>&placa=<?= $cadaCredito["placa"] ?><?= $cadaClasificacion ?>" title="Descarga del producto" <?= $target ?>><img src='Imgs/reportIcon.png' border='0' width='30' height='30'></center></a>
+                    </td>					
+                    <td class="results"><?= $cadaCredito["producto"] ?></td>
+                    <td class="results"><?= $cadaCredito["placa"] ?></td>
+                    <td class="results"><?= $cadaCredito["creditos_consumidos"] ?> Cr&eacute;dito(s)</td>
+                    <td class="results"><?= $cadaCredito["fecha_generacion"] ?></td>
+                    <td class="results"><?= $cadaCredito["fecha_vence"] ?></td>
+                    </tr>					
+                    <?php
+                }
+            ?>		
+            </tbody>	
+    </table>
+</div>
--- Vistas/v_datos_basicos.php
+++ Vistas/v_datos_basicos.php
@@ -1,141 +1,138 @@
-	<script>
-		function enviarForm() {		
-			if(!validarDatosUsuario()) return 0;
-			document.frmAdminUser.submit();
-	    } 
-		
-		// selecciona mpio dado departamento
-		function seleccionarMunicipio(idDepto)	{
-			 $("#selMunicipio").load('viewMunicipios.php?idDepto='+idDepto);
-		};		
-		
-		// seleccion de acuerdo al tipo de documento
-		function cambioCrazon(idTipoDocumento)	{
-			var txtRazon ="";
-			if(idTipoDocumento!=5) {
-				txtRazon += '<input type="text" name="txtNombre" size="25" placeholder="Nombres" value="<?=$listaForm["nombres"]?>" <?=$readonly?>>&nbsp;&nbsp;';
-				txtRazon += '<input type="text" name="txtApellido" size="25" placeholder="Apellidos" value="<?=$listaForm["apellidos"]?>" <?=$readonly?>>';
-			} else {
-				txtRazon += '<input type="text" name="txtNombre" size="55" placeholder="Razón Social" value="<?=$listaForm["razon_social"]?>" <?=$readonly?>>';
-				txtRazon += '<input type="hidden" name="txtApellido" value=""></div>';			
-			}
-				$("#crazon").html(txtRazon);
-		};	
-	</script>
-	
-<form name="frmAdminUser" method="post" action="?mnu=datos_basicos<?php echo $rest = (@$_GET["credits"]==1) ? "&credits=1" : "" ?>">
-	<table width="95%" border="0" cellspacing="0" align="center" class="tableFonts">
-		<tr>
-			<td colspan="2" class="titleSite" align='center'>Datos de Usuario</td>
-		</tr>
-		<tr>
-			<td colspan="2">&nbsp;</td>
-		</tr>	
-		<tr class="results">
-			<td class="results">Tipo de Documento:*</td>
-			<td class="results">
-				<select type="text" id="selTipoDocumento" name="selTipoDocumento" onChange="cambioCrazon(this.value)">
-				<?php
-					if($readonly!="")
-						echo "<option value='{$listaForm["id_tipo_documento"]}' selected>{$listaForm["tipo_documento"]}</option>";
-					else {
-				?>
-						<option value="0">Seleccione Tipo de Documento</option>
-				<?php
-						foreach($identificacion->selectAll() as $cadaDocumento) {
-				?>
-						<option value=<?=$cadaDocumento["id"]?>> <?=($cadaDocumento["nombre"])?> </option>
-					<?php
-						}
-					}
-				?>					
-				</select>				
-			</td>
-		</tr>
-		<tr class="results">
-			<td class="results">N&uacute;mero de Documento:*</td>
-			<td class="results">
-				<input type="text" name="txtDocumento" size="45" value="<?=$listaForm["numero_documento"]?>" readonly />
-			</td>
-		</tr>
-		<tr class="results">
-			<td class="results">Nombre/Raz&oacute;n Social:*</td>
-			<td class="results">
-				<div id="crazon">
-					<input type="text" name="txtNombre" size="25" placeholder="Nombres" value="<?=$listaForm["nombres"]?>" <?=$readonly?>>&nbsp;&nbsp;
-					<input type="text" name="txtApellido" size="25" placeholder="Apellidos" value="<?=$listaForm["apellidos"]?>" <?=$readonly?>>
-				</div>				
-			</td>
-		</tr>
-		<tr class="results">
-			<td class="results">Fecha de Nacimiento:*</td>
-			<td class="results">
-				<input name="txtFechaNacimiento" type="text" size="25" value="<?=$listaForm["fecha_nacimiento"]?>" placeholder="DD/MM/AAAA"/>
-			</td>
-		</tr>		
-		<tr class="results">
-			<td class="results">Correo Electr&oacute;nico:*</td>
-			<td class="results">
-				<input name="buyerEmail" type="text" size="60" value="<?=$listaForm["correo_electronico"]?>" readonly />
-			</td>
-		</tr>	
-		<tr class="results">
-			<td class="results">Tel&eacute;fono:</td>
-			<td class="results">
-				<input type="text" name="txtTelefono" size="25"  value="<?=$listaForm["telefono"]?>">
-			</td>
-		</tr>		
-		<tr class="results">
-			<td class="results">N&uacute;mero Celular:</td>
-			<td class="results">
-				<input type="text" name="txtCelular" size="25" value="<?=$listaForm["celular"]?>">
-			</td>
-		</tr>
-		<tr class="results">
-			<td class="results">Departamento:*</td>
-			<td class="results">
-				<select type="text" id="selDepartamento" name="selDepartamento" onChange="seleccionarMunicipio(this.value)">
-				<?php
-					if($readonly!="")
-						echo "<option value='{$listaForm["id_departamento"]}' selected>{$listaForm["departamento"]}</option>";				
-				?>				
-					<option value="0">Seleccione Departamento</option>
-				
-					<?php
-						foreach($deptos->selectAll() as $cadaDepto) {
-					?>
-						<option value=<?=$cadaDepto["id"]?>> <?=($cadaDepto["nombre"])?> </option>
-					<?php
-						}
-					?>					
-				</select>				
-			</td>
-		</tr>	
-		<tr class="results">
-			<td class="results">Municipio:*</td>
-			<td class="results">
-				<select type="text" id="selMunicipio" name="selMunicipio">
-				<?php
-					if($readonly!="")
-						echo "<option value='{$listaForm["id_municipio"]}' selected>{$listaForm["municipio"]}</option>";				
-				?>							
-					<option value="0">Seleccione Municipio</option>
-				</select>
-			</td>
-		</tr>
-		
-		<tr class="results">
-			<td class="results">Dirección*</td>
-			<td class="results">
-				<input type="text" name="txtDireccion" size="60"  value="<?=$listaForm["direccion"]?>">
-			</td>
-		</tr>	
-		<tr class="results">
-			<td colspan="2" align="center" class="results">
-				<hr size="0">
-				<center><input type="button" name="type" id="cambioClave" title="Aplicar Cambio" value="Guardar Información" onClick="enviarForm()"/></center>
-				<hr size="0">
-			</td>
-		</tr>
-	</table>	
+<script>
+    function enviarForm() {
+        if (!validarDatosUsuario())
+            return 0;
+        document.frmAdminUser.submit();
+        }
+
+    // selecciona mpio dado departamento
+    function seleccionarMunicipio(idDepto) {
+        $("#selMunicipio").load('viewMunicipios.php?idDepto=' + idDepto);
+    }
+    ;
+
+    // seleccion de acuerdo al tipo de documento
+    function cambioCrazon(idTipoDocumento) {
+        var txtRazon = "";
+        if (idTipoDocumento != 5) {
+            txtRazon += '<input type="text" name="txtNombre" size="25" placeholder="Nombres" value="<?= $listaForm["nombres"] ?>" <?= $readonly ?>>&nbsp;&nbsp;';
+            txtRazon += '<input type="text" name="txtApellido" size="25" placeholder="Apellidos" value="<?= $listaForm["apellidos"] ?>" <?= $readonly ?>>';
+        } else {
+            txtRazon += '<input type="text" name="txtNombre" size="55" placeholder="Razón Social" value="<?= $listaForm["razon_social"] ?>" <?= $readonly ?>>';
+            txtRazon += '<input type="hidden" name="txtApellido" value=""></div>';
+        }
+        $("#crazon").html(txtRazon);
+    }
+    ;
+</script>
+
+<form class="form-horizontal" name="frmAdminUser" method="post" action="?mnu=datos_basicos<?php echo $rest = (@$_GET["credits"] == 1) ? "&credits=1" : "" ?>">
+
+    <div class="titleSite" >Datos de Usuario</div><br>
+
+    <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Tipo de Documento:*</label>
+        <div class="col-sm-3">
+            <select class="form-control" type="text" id="selTipoDocumento" name="selTipoDocumento" onChange="cambioCrazon(this.value)">
+                <?php
+                if ($readonly != "")
+                    echo "<option value='{$listaForm["id_tipo_documento"]}' selected>{$listaForm["tipo_documento"]}</option>";
+                else {
+                    ?>
+                    <option value="0">Seleccione Tipo de Documento</option>
+                    <?php
+                    foreach ($identificacion->selectAll() as $cadaDocumento) {
+                        ?>
+                        <option value=<?= $cadaDocumento["id"] ?>> <?= ($cadaDocumento["nombre"]) ?> </option>
+                        <?php
+                    }
+                }
+                ?>					
+            </select>	
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">N&uacute;mero de Documento:*</label>
+        <div class="col-sm-3">
+            <input class="form-control" type="text" name="txtDocumento" size="45" value="<?= $listaForm["numero_documento"] ?>" readonly />
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Nombre/Raz&oacute;n Social:*</label>
+        <div class="col-sm-5">
+            <input class="form-control" type="text" name="txtNombre" size="25" placeholder="Nombres" value="<?= $listaForm["nombres"] ?>" <?= $readonly ?>>&nbsp;&nbsp;            
+        </div>
+        <div class="col-sm-5">
+            <input class="form-control" type="text" name="txtApellido" size="25" placeholder="Apellidos" value="<?= $listaForm["apellidos"] ?>" <?= $readonly ?>>
+        </div>
+        
+    </div>
+
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Fecha de Nacimiento:*</label>
+        <div class="col-sm-3">
+            <input class="form-control" name="txtFechaNacimiento" type="text" size="25" value="<?= $listaForm["fecha_nacimiento"] ?>" placeholder="DD/MM/AAAA"/>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Correo Electr&oacute;nico:*</label>
+        <div class="col-sm-10">
+            <input class="form-control" name="buyerEmail" type="text" size="60" value="<?= $listaForm["correo_electronico"] ?>" readonly />
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Tel&eacute;fono:</label>
+        <div class="col-sm-3">
+            <input class="form-control" type="text" name="txtTelefono" size="25"  value="<?= $listaForm["telefono"] ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">N&uacute;mero Celular:</label>
+        <div class="col-sm-3">
+            <input class="form-control" type="text" name="txtCelular" size="25" value="<?= $listaForm["celular"] ?>">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Departamento:*</label>
+        <div class="col-sm-3">
+            <select class="form-control" type="text" id="selDepartamento" name="selDepartamento" onChange="seleccionarMunicipio(this.value)">
+                <?php
+                if ($readonly != "")
+                    echo "<option value='{$listaForm["id_departamento"]}' selected>{$listaForm["departamento"]}</option>";
+                ?>				
+                <option value="0">Seleccione Departamento</option>
+
+                <?php
+                foreach ($deptos->selectAll() as $cadaDepto) {
+                    ?>
+                    <option value=<?= $cadaDepto["id"] ?>> <?= ($cadaDepto["nombre"]) ?> </option>
+                    <?php
+                }
+                ?>					
+            </select>	
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Municipio:*</label>
+        <div class="col-sm-3">
+            <select class="form-control" type="text" id="selMunicipio" name="selMunicipio">
+                <?php
+                if ($readonly != "")
+                    echo "<option value='{$listaForm["id_municipio"]}' selected>{$listaForm["municipio"]}</option>";
+                ?>							
+                <option value="0">Seleccione Municipio</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Dirección*</label>
+        <div class="col-sm-10">
+            <input class="form-control" type="text" name="txtDireccion" size="60"  value="<?= $listaForm["direccion"] ?>">
+        </div>
+    </div>
+
+    <center><input class="btn btn-success"type="button" name="type" id="cambioClave" title="Aplicar Cambio" value="Guardar Información" onClick="enviarForm()"/></center>
+    <br>
 </form>	
