cc37d6adb4561308a20b0122107dcdd15442df0e
jose luis <jose1914luis@gmail.com>
2017-07-04 15:30:59 -0500

    modificaciones nueva version

--- Correo/Correo.php
+++ Correo/Correo.php
@@ -44,6 +44,49 @@ class Correo {
         return $response;
     }
 
+    public function usuarioNuevo($nombre, $urlActivaUsuario, $email){
+    
+        
+
+	$mensaje = '	
+		<html>
+		<head>
+		<title>SIGMIN :: Creaci�n de Cuenta </title>
+		</head>
+		<body>
+		<table border="0" align="center" width="600">
+		<tr>
+		<td>
+		<center><h1>Bienvenido(a) al Catastro Minero Online, SIGMIN -<h1></center>	
+		<hr size="0">
+
+		<p>Apreciado(a) '.strtoupper(utf8_decode($nombre)).',
+		<p>Con una cuenta en SIGMIN usted tendr&aacute; acceso al Catastro Minero Online, donde encontrar&aacute; la informaci&oacute;n actualizada de solicitudes y t&iacute;tulos mineros de Colombia, con la posibilidad de visualizarlos f&aacute;cilmente sobre los mapas de Google.</p>
+		<p>Para activar su <i><b>CUENTA GRATUITA</b></i> haga <b><a href="http://www.sigmin.co/finder/registrese_en_sigmin.php?'.$urlActivaUsuario.'" style="text-decoration:none;" target="_top">CLICK AQU&Iacute;</a></b></p>
+		<p> RECUERDE: Su usuario (username) de acceso al sistema es: <i><u>'.$email.'</u></i></p>
+		<p>Para nosotros ser&aacute; un placer atenderle, pondremos todo nuestro empe&ntilde;o para garantizarle un buen servicio.</p>
+		<p>Atentamente,</p>
+		<p><b>Jos&eacute; Ernesto C&aacute;rdenas Santamar&iacute;a<br>
+		Director General<br>
+		SIGMIN S.A.S</b><br>		
+		<div style="font-family: Verdana, Arial, Helvetica, sans-serif;	font-size: 11px; background-color: #555555;	color: #ffffff;	text-align: center;">
+			<div>&nbsp;</div>
+			<div>
+				<div>Calle 7 Sur # 42-70 Of 1101, Edificio Forum. Medell&iacute;n-Colombia</div>
+				<div>Tel&eacute;fono: (574) 322 70 04 - M&oacute;vil: 314 716 0680</div>
+				<div><a href="mailto:contactenos@sigmin.com.co" style="text-decoration:none; color:#ffffff">contactenos@sigmin.com.co</a></div>
+				<div>:: SIGMIN S.A.S. 2012 - TODOS LOS DERECHOS RESERVADOS &reg; ::</div>
+			</div>
+			<div style="padding-bottom:10px;">&nbsp;</div>
+		</div>
+
+		</td>
+		</tr>
+		</body>
+		</html>	
+	';		
+    }
+    
     public function recuperarContra($passwd_tmp, $login_tmp, $email_pwd) {
 
         $mensaje = '	
