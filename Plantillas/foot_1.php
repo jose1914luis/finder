116388186bbbd13baa884f2b7a34df8ec707ff59
jose luis <jose1914luis@gmail.com>
2017-07-06 19:00:53 -0500

    modificaciones nueva version

---
+++ Plantillas/foot_1.php
@@ -0,0 +1,6 @@
+<div class="site-footer2">    
+    <div>
+        <div>SIGMIN S.A.S. 2012 - TODOS LOS DERECHOS RESERVADOS Calle 7 Sur # 42-70 Of 1101, Edificio Forum. Medell&iacute;n-Colombia</div>
+        <div>Tel&eacute;fono: (574) 322 70 04 - M&oacute;vil: 314 716 0680 email: contactenos@sigmin.com.co</div>        
+    </div>
+</div>
