63edc0956791a4bfdd02eb27e671656ea813730c
jose luis <jose1914luis@gmail.com>
2017-07-21 14:39:59 -0500

    cambio de ruta

--- Plantillas/menu.php
+++ Plantillas/menu.php
@@ -17,7 +17,7 @@
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            
+
             <form class="navbar-form navbar-left" name="searchWords">
                 <div class="input-group buscador">
                     <input id="txtBusqueda" type="text" class="form-control" placeholder="Palabra Clave...">                    
@@ -30,18 +30,27 @@
                 <?php if (isset($_SESSION['id_usuario'])) { ?>
 
                     <li><a href="?mnu=creditos"> Creditos: <?= $_SESSION['usr_cred']['credito'] ?>$</a></li>
-                    <?php if($_SESSION["pagina"] != "map"){?>
-                    <li><a href="javascript:confirmaCreditoMapa('?pagina=map')"> Mapa</a></li>
-                    <?php } if($_SESSION["pagina"] == "map"){?>
-                    <li role="presentation" class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Análisis <span class="caret"></span></a>
-                        <ul class="dropdown-menu">                                    
-                            <li><a href="javascript:" onclick="Pros_Open('generarArea')">Generar Área Libre</a></li>
-                            <li><a href="javascript:" onclick="Pros_Open('LiberarArea')">Alerta Liberación de Área</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="javascript:" onclick="Pros_Open('Perimetral')">Analisis Perimetral</a></li>
-                        </ul>
-                    </li>
+                    <?php if ($_SESSION["pagina"] != "map") { ?>
+                        <li><a href="javascript:confirmaCreditoMapa('?pagina=map')"> Mapa</a></li>
+                    <?php } if ($_SESSION["pagina"] == "map") { ?>                     
+
+                        <li class="menu-item dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Análisis <b class="caret"></b></a>
+                            <ul class="dropdown-menu">                               
+                                <li class="menu-item dropdown dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Polígono <i class="fa fa-square-o" aria-hidden="true"></i></a>
+                                    <ul class="dropdown-menu">
+                                        <li class="menu-item "><a href="javascript:"  id="poligono" onClick="Pros_Open('generarArea');toggleControl(this);" >Dibujar <i class="fa fa-pencil" aria-hidden="true"></i></a></li>
+                                        <li class="menu-item "><a href="javascript:" onClick="Pros_Open('generarArea');mostrardiv('freeGeneratorArea_coordinates')">Coordenadas <i class="fa fa-map-o" aria-hidden="true"></i></a></li>                                       
+                                    </ul>
+                                </li>
+                                 <li class="menu-item dropdown dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Perímetro <i class="fa fa-dot-circle-o" aria-hidden="true"></i></a>
+                                    <ul class="dropdown-menu">
+                                        <li class="menu-item "><a href="javascript:"  id="area" value="point"onclick="Pros_Open('Perimetral');toggleControl(this);">Ubicar Punto <i class="fa fa-location-arrow" aria-hidden="true"></i></a></li>
+                                        <li class="menu-item "><a href="javascript:" onclick="Pros_Open('Perimetral');mostrardiv('point_coordinates')">Coordenadas <i class="fa fa-map-o" aria-hidden="true"></i></a></li>                                       
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+
                     <?php } ?>
                     <li role="presentation" class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Propiedades Mineras <span class="caret"></span></a>
@@ -60,7 +69,7 @@
                             <li><a href="?mnu=creditos">Creditos</a></li>
                             <li><a href="?mnu=datos_basicos">Datos Personales</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="?mnu=logout">Salir</a></li>
+                            <li><a href="?mnu=logout">Logout <i class="fa fa-power-off padding-left-ten-px red-text"></i></a></li>
                         </ul>
                     </li>
 
