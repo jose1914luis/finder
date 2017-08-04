5c38840686d488f208a9fdbdff588dd3d4bd1711
jose luis <jose1914luis@gmail.com>
2017-08-04 15:16:10 -0500

    cambio de ruta

--- Vistas/v_descargas.php
+++ Vistas/v_descargas.php
@@ -2,8 +2,8 @@
 <?php
 @$valorCreditoExpediente = @$credito;
 ?>
-<div style="overflow: auto; height: 100%; width: 100%">
-
+<div id="table-wrapper">
+    <!--<div style="overflow: auto; height: 100%; width: 100%">-->
     <form name="frmDescargas" method="POST" action="?mnu=descargas">
         <table width="95%" align="center">
             <tr>
@@ -19,7 +19,7 @@
             </tr>
             <tr>
                 <td>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<textarea name="txtPlacas" rows="4" cols="60"><?= @$_POST["txtPlacas"] ?></textarea><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<textarea name="txtPlacas" rows="4" cols="100"><?= @$_POST["txtPlacas"] ?></textarea><br>
                     <hr size="1">
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" name="valida_lista" value="Validar Placas" class="btn btn-success"/>
                     <hr size="1">
@@ -32,9 +32,34 @@
                         $contarCreditos = 0;
                         ?>
                         <span class="tableFonts"><b>Resultado validaci&oacute;n del proceso</b></span><br>
-                        <hr size="1">						
-                        <table width="600" align="center" class="results">
+                        <hr size="1">	
 
+                        <table width="100%" align="center" class="results">
+
+                            <thead>
+                                <tr  class="results">	
+                                    <th class="results"><b>Placa</b></th>
+                                    <th class="results"><b>Tipo Expediente</b></th>
+                                    <th class="results"><b>Estado Jur&iacute;dico</b></th>
+                                    <th class="results"><b>Pol&iacute;gono Vigente</b></th>								
+                                </tr>						
+                            </thead>
+
+                            <?php
+                            foreach ($listaValidada as $cadaPlaca) {
+                                if ($cadaPlaca["tipo_expediente"] != "Placa no definida")
+                                    $contarCreditos++;
+                                ?>
+
+                                <tr class="results">	
+                                    <td class="results"><?= $cadaPlaca["placa_usr"] ?></td>
+                                    <td class="results"><?= $cadaPlaca["tipo_expediente"] ?></td>
+                                    <td class="results"><?= $cadaPlaca["estado_juridico"] ?></td>
+                                    <td class="results"><?= $cadaPlaca["poligono_vigente"] ?></td>
+                                </tr>
+                                <?php
+                            }
+                            ?>
                             <tr class="results">
                                 <td colspan="4">
                             <center>
@@ -48,34 +73,14 @@
                     </td>
                 </tr>
 
-                <tr  class="results">	
-                    <th class="results"><b>Placa</b></th>
-                    <th class="results"><b>Tipo Expediente</b></th>
-                    <th class="results"><b>Estado Jur&iacute;dico</b></th>
-                    <th class="results"><b>Pol&iacute;gono Vigente</b></th>								
-                </tr>						
-                <?php
-                foreach ($listaValidada as $cadaPlaca) {
-                    if ($cadaPlaca["tipo_expediente"] != "Placa no definida")
-                        $contarCreditos++;
-                    ?>
+            </table>
 
-                    <tr class="results">	
-                        <td class="results"><?= $cadaPlaca["placa_usr"] ?></td>
-                        <td class="results"><?= $cadaPlaca["tipo_expediente"] ?></td>
-                        <td class="results"><?= $cadaPlaca["estado_juridico"] ?></td>
-                        <td class="results"><?= $cadaPlaca["poligono_vigente"] ?></td>
-                    </tr>
-                    <?php
-                }
-                ?>
-
-            </table>						
             <?php
         }
         ?>
         </td>
         </tr>
+
         </table>
 
     </form>
