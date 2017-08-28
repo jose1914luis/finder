557f0679a504a551f1ce0d13143822e7f281336d
jose luis <jose1914luis@gmail.com>
2017-08-28 17:36:38 -0500

    cambio de ruta

--- Plantillas/head.php
+++ Plantillas/head.php
@@ -1,5 +1,5 @@
 <?php
-$_VERSION = 444440;
+$_VERSION = 44444010;
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1">
