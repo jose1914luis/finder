5e621e4354177757559041ffb638e94bd3da7a31
jose luis <jose1914luis@gmail.com>
2017-09-12 13:48:18 -0500

    initial commit

--- sitemap_gen.py
+++ sitemap_gen.py
@@ -218,7 +218,7 @@ def parsePages(startUrl, maxUrls, blockExtensions):
             pageMap[url] = date
             parser = MyHTMLParser(pageMap, redirects, url, maxUrls, blockExtensions, robotParser)
             try:
-                print page
+#                print page
                 parser.feed(page)
                 parser.close()
             except HTMLParseError:
