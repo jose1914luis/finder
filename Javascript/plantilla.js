825490577222e3456858b266335a9c3077f8cb0d
jose luis <jose1914luis@gmail.com>
2017-09-14 12:44:28 -0500

    initial commit

--- Javascript/plantilla.js
+++ Javascript/plantilla.js
@@ -3,8 +3,8 @@ var controlConfig = 1, divAnterior = "creditos";
 
 function confirmaCreditoMapa(url) {
     if (confirm("Visualizar el Mapa equivale a 1 Cr\u00E9dito, Desea consumir Cr\u00E9ditos?"))
-        document.location.href = '?pagina=map';
+        document.location.href = '/?pagina=map';
     else
-        document.location.href = '?pagina=account';
+        document.location.href = '/?pagina=account';
 }
                 
