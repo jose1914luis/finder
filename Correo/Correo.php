6b11e16546a1bbce5ac444db686802341af726dd
jose luis <jose1914luis@gmail.com>
2017-10-04 14:36:47 -0500

    initial commit

--- Correo/Correo.php
+++ Correo/Correo.php
@@ -72,7 +72,7 @@ class Correo {
 		<div style="font-family: Verdana, Arial, Helvetica, sans-serif;	font-size: 11px; background-color: #555555;	color: #ffffff;	text-align: center;">
 			<div>&nbsp;</div>
 			<div>
-				<div>Calle 7 Sur # 42-70 Of 1101, Edificio Forum. Medell&iacute;n-Colombia</div>
+				<div>Calle. 5a # 43b-25 Of 301, Edificio Meridian. Medellín-Colombia</div>
 				<div>Tel&eacute;fono: (574) 322 70 04 - M&oacute;vil: 314 716 0680</div>
 				<div><a href="mailto:contactenos@sigmin.com.co" style="text-decoration:none; color:#ffffff">contactenos@sigmin.com.co</a></div>
 				<div>:: SIGMIN S.A.S. 2012 - TODOS LOS DERECHOS RESERVADOS &reg; ::</div>
