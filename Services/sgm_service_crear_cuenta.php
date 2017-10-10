35f47fc3ca8d141618bf009bd6425001cbaf06cd
jose luis <jose1914luis@gmail.com>
2017-10-10 16:09:44 -0500

    initial commit

--- Services/sgm_service_crear_cuenta.php
+++ Services/sgm_service_crear_cuenta.php
@@ -5,24 +5,15 @@ header('Access-Control-Allow-Origin: *');
 require '../Acceso/Config.php';
 require_once("../Modelos/Usuarios_SGM.php");
 require_once("../Utilidades/LibCurl.php");
+include '../Correo/Correo.php';
 
 $createUsr = new Usuarios_SGM();
 $msgCreate = $createUsr->insertUsrTmpAll($_GET);
 if ($msgCreate == 'OK') {
     $serial = $createUsr->getSerialbyEmail($_GET["txtEmail"]);
-
-    $url = "http://www.sigmin.com.co/EmailServices/sendEmail.php";
-    $params = array(
-        'email' => $_GET["txtEmail"],   
-        'identificacion' => $_GET["txtDocumento"],
-        'nombre' => $_GET["txtNombre"],
-        'codigoVerificacion' => $serial,
-        'urlActivaUsuario' => "email={$_GET["txtEmail"]}&identificacion={$_GET["txtDocumento"]}&codigo_verificacion=" . $serial
-    );
-
-    $connCurl = new LibCurl;
-    $resultado = $connCurl->curl_download($url, $params);
-    $emailRs = json_decode($resultado, true);
+    
+    $correo = new Correo();
+    $correo->usuarioNuevo($_GET["txtNombre"], "email={$_GET["txtEmail"]}&identificacion={$_GET["txtDocumento"]}&codigo_verificacion=" . $serial, $_GET["txtEmail"]);                                                         
 
     echo 1;
 } else {
